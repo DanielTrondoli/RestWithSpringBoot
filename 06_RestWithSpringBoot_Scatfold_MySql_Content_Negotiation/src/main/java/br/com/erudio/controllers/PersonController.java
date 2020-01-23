685fa6bb5fddb0b95f91ff1c464c12a9f45bf6b7
Daniel Trondoli <daniel.de-paula-trondoli@dxc.com>
2020-01-23 17:30:02 -0300

    sessao 13 projeto com Content Negotiation(json, xml)

---
+++ 06_RestWithSpringBoot_Scatfold_MySql_Content_Negotiation/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,52 @@
+package br.com.erudio.controllers;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.model.Person;
+import br.com.erudio.service.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices pService;
+	
+	@GetMapping(value = "/{id}")
+	public PersonVO findById(@PathVariable(value="id") Long id) throws Exception {						
+		return pService.findById(id);		
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable(value="id") Long id) throws Exception {						
+		pService.delete(id);		
+	}
+	
+	@GetMapping
+	public List<PersonVO> findAll() throws Exception {		
+		return pService.findAll();	
+	}
+	
+	@PostMapping
+	public PersonVO create(@RequestBody PersonVO p) throws Exception {		
+		return pService.create(p);	
+	}
+	
+	@PutMapping
+	public PersonVO update(@RequestBody PersonVO p) throws Exception {		
+		return pService.update(p);	
+	}
+	
+}
---
+++ 06_RestWithSpringBoot_Scatfold_MySql_Content_Negotiation/src/main/java/br/com/erudio/service/PersonServices.java
@@ -0,0 +1,54 @@
+package br.com.erudio.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.model.Person;
+import br.com.erudio.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+
+	@Autowired
+	PersonRepository repository;
+
+	public PersonVO create(PersonVO p) {
+		Person entity = DozerConverter.parseObject(p, Person.class);
+		PersonVO vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+
+	public PersonVO update(PersonVO p) {
+
+		Person entity = repository.findById(p.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No data Found for this Id !!"));
+
+		entity.setName(p.getName());
+		entity.setLastName(p.getLastName());
+		entity.setAddres(p.getAddres());
+		entity.setGender(p.getGender());
+
+		return DozerConverter.parseObject(repository.save(entity),PersonVO.class);
+	}
+
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No data Found for this Id !!"));
+		repository.delete(entity);
+	}
+
+	public PersonVO findById(Long id) {
+
+		return DozerConverter.parseObject(repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No data Found for this Id !!")), PersonVO.class);
+
+	}
+
+	public List<PersonVO> findAll() {
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+}
